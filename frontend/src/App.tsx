e5ba98d51330c0d307d33e5e6166315a40389dda
Will Koster <jameswk2@illinois.edu>
2019-02-26 16:51:26 -0600

    Play with some things
    Now it will print more stuff out, which is cool

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type State = Readonly<typeof initialState>
+type AppProps = Readonly<{}>;
+const initialState = {people: new Map<string, number>()};
 class App extends Component {
-    constructor(props: Object) {
+    readonly state: State = initialState;
+    constructor(props: AppProps) {
         super(props);
-        this.state = {people: {}};
         fetch("/api/")
             .then((resp) => resp.json())
-        .then(jsonObj => this.setState({people: jsonObj}));
+            .then((jsonObj: Object) => {
+                this.setState({people: new Map(Object.entries(jsonObj))});
+            });
     }
   render() {
+      let peoplePart: React.ReactElement[] | null;
+      if(this.state["people"].size === 0) {
+          peoplePart = null; ;
+      } else {
+          peoplePart = this.mapOverMap(this.state["people"], (k, v) => <p key={k}>{k}: {v}</p>);
+      }
     return (
       <div className="App">
           <h1>
@@ -19,9 +30,20 @@ class App extends Component {
           <h2>
               How can dogs be real if our eyes aren't real?
           </h2>
+          {peoplePart === null ? <img src="https://media.giphy.com/media/3o7bu8sRnYpTOG1p8k/source.gif" height="20" width="20"/> : peoplePart}
       </div>
     );
   }
+
+    mapOverMap<K, V, O>(m: Map<K, V>, f: (key: K, value: V) => O): O[] {
+        let out: O[] = [];
+        for(let pair of m) {
+            let key = pair[0];
+            let value = pair[1];
+            out.push(f(key, value));
+        }
+        return out;
+    }
 }
 
 export default App;
---
+++ migrations/20190226164555_show_a_sample_migration.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS people (name VARCHAR(30), age SMALLINT);
+INSERT INTO people (name, age) VALUES ('John Cleese', 100), ('Douglas Crockford', 862);
