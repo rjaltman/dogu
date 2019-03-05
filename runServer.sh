c2aba44b3e90fb70c3e5352d56b24414259797aa
Will Koster <jameswk2@illinois.edu>
2019-03-04 22:33:44 -0600

    Add more error checking to runServer

--- runServer.sh
+++ runServer.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 set -e
+if ! (which flask > /dev/null); then
+    echo Flask isn\'t installed
+    exit 1
+fi
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 export FLASK_ENV=development
