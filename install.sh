81f9ded0567caa778f8875c66f6701bfa7dd9009
Will Koster <jameswk2@illinois.edu>
2019-03-07 19:17:47 -0600

    Make install script a little bit more comprehensive

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
+#! /bin/bash
 set -e
 echo "I hope you installed postgres first!"
-sudo -u postgres createdb dogu
-sudo -u postgres createuser dogu
-sudo -u postgres psql -c "ALTER USER dogu LOGIN PASSWORD 'password';"
+if [[ "$OSTYPE" == "linux-gnu" ]] ; then
+    sudo -u postgres createdb dogu
+    sudo -u postgres createuser dogu
+    sudo -u postgres psql -c "ALTER USER dogu LOGIN PASSWORD 'password';"
+elif [[ "$OSTYPE" == "darwin" ]]; then
+    echo "O geez you're on a Mac. I hope these commands work. And you've figured out how to start postgres"
+    createdb dogu
+    createuser dogu
+    psql postgres psql -c "ALTER USER dogu LOGIN PASSWORD 'password';"
+else
+    echo "O Christ, are you on Windows? I hope you're on good terms with your god."
+    echo ">> Exits, does nothing"
+    exit 1
+fi
 
 ./migrate.py migrate
