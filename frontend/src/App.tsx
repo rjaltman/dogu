57266b306f29da91142c692d727c93fbf154a9ce
Will Koster <jameswk2@illinois.edu>
2019-03-11 14:42:41 -0500

    Fiddle more
    I might have to just burn this branch, but it's been a learning
    experience.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,13 @@ class App extends Component {
       } else {
           peoplePart = this.state.people.map(([k, v]) => <p onClick={this.deletePerson.bind(this, k)} key={k}>{k}: {v}</p>);
       }
+
+      let gridStyle: (row: number, column: number) => React.CSSProperties = (row, column) => ({
+          gridColumnStart: column,
+          grodColumnEnd: "span 1",
+          gridRowStart: row,
+          gridRowEnd: "span 1",
+      });
     return (
       <div className="App">
           <h1>
@@ -26,12 +33,18 @@ class App extends Component {
           <h2>
               How can dogs be real if our eyes aren't real?
           </h2>
-          Name: <input value={this.state.nameValue} onChange={(e) => this.setState({nameValue: e.target.value})} />
+        <div onKeyPress={this.handleKeypress.bind(this)} style={{display: "inline-grid", gridTemplateRows: "repeat(3, auto)", gridTemplateColumns: "repeat(2,auto)", width: "auto"}}>
+          <span className="rightAlign" style={gridStyle(1,1)}> Name:</span>
+        <input style={gridStyle(1,2)} value={this.state.nameValue} onChange={(e) => this.setState({nameValue: e.target.value})} />
+
           <br />
-          Age: <input value={this.state.ageValue} onChange={(e) => this.setState({ageValue: e.target.value})} />
+
+          <span className="rightAlign" style={gridStyle(2,1)}>Age:</span> 
+        <input style={gridStyle(2,2)} value={this.state.ageValue} onChange={(e) => this.setState({ageValue: e.target.value})} />
           <div onClick={this.sendNewName.bind(this)}>
               Make a person!
           </div>
+        </div>
           {peoplePart === null ? <img src="https://media.giphy.com/media/3o7bu8sRnYpTOG1p8k/source.gif" height="20" width="20"/> : peoplePart}
       </div>
     );
@@ -48,18 +61,22 @@ class App extends Component {
     mapOverMap<K, V, O>(m: Map<K, V>, f: (key: K, value: V) => O): O[] {
         let out: O[] = [];
         for(let pair of m) {
-            let key = pair[0];
-            let value = pair[1];
+            let key: K = pair[0];
+            let value: V = pair[1];
             out.push(f(key, value));
         }
         return out;
     }
 
   sendNewName() {
+      if(this.state.nameValue === "" && this.state.ageValue === "")
+          return;
       let name = this.state.nameValue;
       let age = parseInt(this.state.ageValue);
+
       let payload = {name, age};
       let headers = {"Content-Type": "application/json"};
+      this.setState({ageValue: "", nameValue: ""});
       fetch("api/insert", {method: "POST", body: JSON.stringify(payload), headers})
           .then(res => res.json())
           .then(jsonObj => { 
@@ -80,6 +97,14 @@ class App extends Component {
               if(jsonObj["status"] === "success")
                   this.refreshPeople();
           });
+  }
+  
+  handleKeypress<El>(e: React.KeyboardEvent<El>) {
+      const ENTER_KEY = 13;
+      let keyCode: number = e.which;
+      if(keyCode == ENTER_KEY) {
+          this.sendNewName();
+      }
   }
 }
 
