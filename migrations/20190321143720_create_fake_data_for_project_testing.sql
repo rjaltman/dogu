20cc3a15bd763fa2443f2ae7ecd92ffb90448ec0
Will Koster <jameswk2@illinois.edu>
2019-03-21 15:01:46 -0500

    Add migration for search testing
    It might not work. Be careful.

---
+++ migrations/20190321143720_create_fake_data_for_project_testing.sql
@@ -0,0 +1,14 @@
+INSERT INTO organization (name) VALUES ('A FAKE ORGANIZATION');
+
+INSERT INTO project (organization_id, university_id, name, description)
+VALUES 
+((SELECT id FROM organization WHERE name = 'A FAKE ORGANIZATION'), (SELECT id FROM university WHERE name = 'A FAKE UNIVERSITY'), 'Fake project 1', 'This is a complicated project involving deep learning, NoSQL, and other buzzwords that will certainly attract developers who call themselves "ninjas."'), 
+((SELECT id FROM ORGANIZATION WHERE name = 'A FAKE ORGANIZATION'), 'Fake project 2', 'This is a regular project involving, NoSQL, and technologies that will probably just work properly.');
+
+INSERT INTO project_tags (project_id, tag) VALUES 
+((SELECT id FROM project WHERE name = 'Fake project 1'), 'AI'), 
+((SELECT id FROM project WHERE name = 'Fake project 1'), 'NoSQL'), 
+((SELECT id FROM project WHERE name = 'Fake project 1'), 'Web scale'), 
+((SELECT id FROM project WHERE name = 'Fake project 2'), 'Java'), 
+((SELECT id FROM project WHERE name = 'Fake project 2'), 'MySQL'), 
+((SELECT id FROM project WHERE name = 'Fake project 2'), 'Apache');
