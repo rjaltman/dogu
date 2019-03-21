db40980e61f27d6a0ef6b45bb1c45c95d0dfc5b9
Will Koster <jameswk2@illinois.edu>
2019-03-21 15:26:23 -0500

    Fix fake project migration

--- migrations/20190321143720_create_fake_data_for_project_testing.sql
+++ migrations/20190321143720_create_fake_data_for_project_testing.sql
@@ -2,8 +2,8 @@ INSERT INTO organization (name) VALUES ('A FAKE ORGANIZATION');
 
 INSERT INTO project (organization_id, university_id, name, description)
 VALUES 
-((SELECT id FROM organization WHERE name = 'A FAKE ORGANIZATION'), (SELECT id FROM university WHERE name = 'A FAKE UNIVERSITY'), 'Fake project 1', 'This is a complicated project involving deep learning, NoSQL, and other buzzwords that will certainly attract developers who call themselves "ninjas."'), 
-((SELECT id FROM ORGANIZATION WHERE name = 'A FAKE ORGANIZATION'), 'Fake project 2', 'This is a regular project involving, NoSQL, and technologies that will probably just work properly.');
+((SELECT id FROM organization WHERE name = 'A FAKE ORGANIZATION'), (SELECT id FROM university WHERE name = 'NOT REAL SCHOOL'), 'Fake project 1', 'This is a complicated project involving deep learning, NoSQL, and other buzzwords that will certainly attract developers who call themselves "ninjas."'), 
+((SELECT id FROM ORGANIZATION WHERE name = 'A FAKE ORGANIZATION'), (SELECT id FROM university WHERE name = 'NOT REAL SCHOOL'), 'Fake project 2', 'This is a regular project involving, NoSQL, and technologies that will probably just work properly.');
 
 INSERT INTO project_tags (project_id, tag) VALUES 
 ((SELECT id FROM project WHERE name = 'Fake project 1'), 'AI'), 
