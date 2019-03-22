8bbaa2ad65619e26e48ac652e7a34564f4d3f2de
Will Koster <jameswk2@illinois.edu>
2019-03-21 19:03:59 -0500

    Improve install file a little
    I might need this for testing later

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ set -e
 echo "I hope you installed postgres first!"
 if [[ "$OSTYPE" == "linux-gnu" ]] ; then
     sudo -u postgres createdb dogu
-    sudo -u postgres createuser dogu
+    sudo -u postgres createuser -d dogu
     sudo -u postgres psql -c "ALTER USER dogu LOGIN PASSWORD 'password';"
 elif [[ "$OSTYPE" == "darwin" ]]; then
     echo "O geez you're on a Mac. I hope these commands work. And you've figured out how to start postgres"
     createdb dogu
-    createuser dogu
+    createuser -d dogu
     psql postgres psql -c "ALTER USER dogu LOGIN PASSWORD 'password';"
 else
     echo "O Christ, are you on Windows? I hope you're on good terms with your god."
