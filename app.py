3b3664a6d417cda7c22f65d793cf183ca7e74006
Justin Ruan <justinr3@illinois.edu>
2019-04-11 12:02:49 -0500

    Added getRecommendations to api

--- app.py
+++ app.py
@@ -152,6 +152,47 @@ def search():
 
     return jsonify({"success": True, "projects": projectsToShow})
 
+@app.route("/api/recommendations", methods=["GET"])
+def getRecommendations():
+    username = session.get('username', None)
+    with conn.cursor(cursor_factory=RealDictCursor) as c:
+        if not username:
+            projectsToShow = []
+        else:
+            c.execute("SELECT EXISTS (SELECT 1 FROM account WHERE username = %s)", (username, ))
+            if not all(c.fetchone().values()):
+                raise Exception("You have a cookie from a user who doesn't exist!")
+            c.execute(("WITH my_account_id  AS ("
+                "SELECT account.account_id"
+                "FROM account"
+                "WHERE username = %(username)s"
+                "), my_preferences AS ("
+                "SELECT project_id"
+                "FROM preference"
+                "WHERE preference.account_id IN my_account_id"
+                "), related_tags AS ("
+                "SELECT project_tags.tag"
+                "FROM my_preferences"
+                "INNER JOIN project_tags ON preference.project_id=project_tags.project_id"
+                "), projects_same_tag AS ("
+                "SELECT project_id"
+                "FROM project_tags"
+                "WHERE project_tags.tag IN (SELECT tag FROM related_tags)"
+                ")"
+                "SELECT *"
+                "FROM project"
+                "WHERE (status='New') AND id IN ("
+                    "SELECT project_id"
+                    "FROM projects_same_tag"
+                    "EXCEPT"
+                        "(SELECT project_id"
+                        "FROM preference"
+                        "WHERE preference.account_id IN my_account_id))"),
+                {"username": username})
+            projectsToShow = list(c)
+
+    return jsonify({"success": True, "projects": projectsToShow})
+
 @app.route("/api/project/<int:id>", methods=["GET"])
 def getProject(id):
     with conn.cursor(cursor_factory=RealDictCursor) as c:
